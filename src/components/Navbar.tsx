dd851d3f250449260f05132fd26cc2a1abe11998
Jose Vitriago <josevicentevitriago12@gmail.com>
2025-11-03 21:25:42 -0300

    Update Navbar component and styles
    Refactored imports in Navbar.tsx and updated Navbar.module.css for improved styling. Also updated Vite dependency chunks and metadata to reflect changes in React and React DOM modules.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,57 @@
-import Logo from "../assets/marcaGravX.png"
-import style from "./Style/Navbar.module.css"
-import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import { Link } from "react-router"
+import Logo from "../assets/marcaGravX.png";
+import style from "./Style/Navbar.module.css";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.style.overflow = isOpen ? "hidden" : "auto";
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [isOpen]);
+
     return (
-
-        <header>
-            <nav>
+        <header className={style.header}>
+            <div className={style.navbar}>
                 <div className={style.imageLogo}>
-                   <Link to="/"><img src={Logo} alt="Logo" /></Link> 
+                    <Link to="/">
+                        <img src={Logo} alt="Logo GravX" />
+                    </Link>
                 </div>
 
-                <div className={style.Links}>
-                    <li><Link to="/">Inicio</Link></li> 
-                    <li><Link to="/Nutrition">Alimentos</Link></li>
-                    <li><Link to="/Workouts">Rutinas</Link></li>
-                    <li><Link to="/Calculator">Calculadora</Link></li>
+                {/* Links principales */}
+                <div
+                    className={`${style.menuLinks} ${isOpen ? style.open : ""}`}
+                    onClick={() => setIsOpen(false)}
+                >
+                    <ul className={style.Links}>
+                        <li><Link to="/">Inicio</Link></li>
+                        <li><Link to="/Nutrition">Alimentos</Link></li>
+                        <li><Link to="/Workouts">Rutinas</Link></li>
+                        <li><Link to="/Calculator">Calculadora</Link></li>
+                    </ul>
                 </div>
+                <ul className={style.LinksDate}>
+                    <li><Link to="/Login">Iniciar Sesión</Link></li>
+                    <li><Link to="/Register">Regístrate</Link></li>
+                </ul>
 
-                <div className={style.LinksDate}>
-                    <li><Link to="/Login">Iniciar Sesion </Link></li>
-                    <li><Link to="/Register">Registrate</Link></li>
+
+                {/* Botón hamburguesa */}
+                <div
+                    className={`${style.itemToggle} ${isOpen ? style.open : ""}`}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
-
-            </nav>
+            </div>
         </header>
-
     );
-}
+};
 
 export default Navbar;
