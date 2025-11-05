7ca6fff41ddbb4bad564284d18eb5baf0aed5796
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2025-11-05 12:42:07 -0300

    Replace LightRays with Aurora effect on home page
    Removed LightRays component and its related CSS, added Aurora component and styles. Updated home page to use Aurora with new color stops and parameters. Adjusted Navbar and Home styles for improved layout and appearance.

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,27 +1,24 @@
-import LightRays from "../components/LightRays";
-
+import Aurora from "../components/Aurora";
+import style from "./Style/Home.module.css"
 
 
 const Home = () => {
-    return (
-        <>
 
-            <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-                <LightRays
-                    raysOrigin="top-center"
-                    raysColor="#580209ff"
-                    raysSpeed={1.5}
-                    lightSpread={0.8}
-                    rayLength={1.2}
-                    followMouse={true}
-                    mouseInfluence={0.1}
-                    noiseAmount={0.1}
-                    distortion={0.05}
-                    className="custom-rays"
+
+    return (
+
+        <div className={style.homeBody}>
+
+            <div style={{width: "100%", height: "800px"}}>
+                <Aurora
+                    colorStops={["#9C1107", "#0C0A09", "#9C1107"]}
+                    blend={0.5}
+                    amplitude={1.0}
+                    speed={0.5}
                 />
             </div>
-
-        </>
+              
+        </div>
     );
 }
 
