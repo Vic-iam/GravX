97286d2892e1570be033404c25f0903bdaefd6d9
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2025-11-14 11:53:07 -0300

    Enhance input placeholders and update styles
    Added placeholder text to registration form inputs for better user guidance. Improved input and button styling in Admin and Login modules by adjusting padding, font size, opacity, and hover/active states for a more consistent UI.

--- src/utils/Register.tsx
+++ src/utils/Register.tsx
@@ -40,10 +40,10 @@ const Register = () => {
 
                     <div className={style.adminIndex}>
                         <label>Nombre</label>
-                        <input name="nombre"  onChange={handleChange} />
+                        <input name="nombre" placeholder="Escribe tu nombre" onChange={handleChange} />
 
                         <label>Correo electronico</label>
-                        <input name="email" onChange={handleChange} />
+                        <input name="email" placeholder="Escribe tu correo electronico" onChange={handleChange} />
 
                         <label>Contraseña</label>
                         <input name="passoword" placeholder="Escribe tu contrasena" onChange={handleChange}/>
