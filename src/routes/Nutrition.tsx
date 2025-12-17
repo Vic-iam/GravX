9c98dbe6c2544191566bc1d13923f309ee779b6c
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2025-12-17 14:01:31 -0300

    Add Nutrition page with styled layout and Aurora effect
    Introduced a new Nutrition page using a dedicated CSS module for styling. The page features a presentation section with an Aurora background effect and a container for introductory text about nutrition.

--- src/routes/Nutrition.tsx
+++ src/routes/Nutrition.tsx
@@ -1,11 +1,33 @@
-
+'use client'
+import React from "react";
+import Aurora from "../components/Aurora";
+import style from "./Style/Nutrition.module.css"
 
 
 const  Nutrition = () => {
     return (
-        <>
+        <div className={style.nutritionBody}>
+
+            <section className={style.presentationNutrition}>
+                <div className={style.auroraBg}>
+                    <Aurora
+                        colorStops={["#9C1107", "#0C0A09", "#9C1107"]}
+                        blend={0.5}
+                        amplitude={1.0}
+                        speed={0.5}
+                    />
+                </div>
+
+                <div className={style.nutritionContainer}>
+                    <h1>Alimentancion</h1>
+                    <p>
+                        "Aqui podras descubrir e organizar tu alimentación"
+                    </p>
+                </div>
+
+            </section>
         
-        </>
+        </div>
     );
 }
 
