bb33c348ea113c784720050afd16628a286f9ac0
Jose Vitriago <80923963+Vic-iam@users.noreply.github.com>
2025-12-29 12:39:54 -0300

    Add GitHub Octicons to nutrition page
    Added react-icons/go dependency and updated nutrition page to use GitHub Octicons. Adjusted nutrition data and updated related styles for improved iconography and layout.

--- src/routes/Nutrition.tsx
+++ src/routes/Nutrition.tsx
@@ -1,9 +1,11 @@
 'use client'
+import { Link } from "react-router";
 import Aurora from "../components/Aurora";
 import style from "./Style/Nutrition.module.css"
+import { GoChevronDown } from "react-icons/go";
 
 
-const  Nutrition = () => {
+const Nutrition = () => {
     return (
         <div className={style.nutritionBody}>
 
@@ -18,14 +20,44 @@ const  Nutrition = () => {
                 </div>
 
                 <div className={style.nutritionContainer}>
-                    <h1>Alimentancion</h1>
+                    <h1>La Alimentancion</h1>
                     <p>
-                        "Descubre y organiza tu alimentación"
+                        "Es una de la cosas mas importante de la vida"
+                    </p>
+                    <p>
+                        Descubre y organiza tu alimentancion contando las calorias,carbohidratos y proteinas
                     </p>
                 </div>
 
             </section>
-        
+
+            <section className={style.listContainer}>
+
+                <div className={style.titleStyle}>
+                    <div className={style.listTitle}>
+                        <h2>Lista de alimentos</h2>
+                    </div>
+                    <Link to="/Nutrition" className={style.iconStyle}>
+                        <GoChevronDown />
+                    </Link>
+                </div>
+
+                <div className={style.alimentosContainer}>
+
+                    <div className={style.alimentoIndex}>
+                        <label>Busca el alimento</label>
+                        <input type="search" placeholder="Escribe el alimento"></input>
+                    </div>
+
+                    <div className={style.listaAlimentos}>
+                        
+                    </div>
+
+                </div>
+
+
+            </section>
+
         </div>
     );
 }
