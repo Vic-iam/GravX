356de47055f333f2c8f77838d40f2aff73762786
Vic-iam <80923963+Vic-iam@users.noreply.github.com>
2026-01-21 20:41:31 -0300

    Add styles and layout for Calculator and Workouts pages
    Introduced Calculator.module.css and Workouts.module.css for dedicated page styling. Updated Calculator.tsx and Workouts.tsx to use new styles and added Aurora background and structured containers for improved UI presentation.

--- src/routes/Calculator.tsx
+++ src/routes/Calculator.tsx
@@ -1,13 +1,34 @@
 
-
+import Aurora from "../components/Aurora";
+import style from "./Style/Calculator.module.css";
 
 
 const  Calculator = () => {
     return (
-        <>
-        
-        </>
+
+        <div className={style.calculatorBody}>
+
+          <section className={style.presentationCalculator}>
+            <div className={style.auroraBg}>
+              <Aurora
+                colorStops={["#9C1107", "#0C0A09", "#9C1107"]}
+                blend={0.5}
+                amplitude={1.0}
+                speed={0.5}
+              />
+            </div>
+
+            <div className={style.calculatorContainer}>
+
+                <h1>Calculadora de Gravedad</h1>
+
+            </div>
+
+          </section>
+
+        </div>
+
     );
 }
 
-export default Calculator ;
+export default Calculator;
--- src/routes/Workouts.tsx
+++ src/routes/Workouts.tsx
@@ -1,12 +1,32 @@
+import style from "./Style/Workouts.module.css"
+import Aurora from "../components/Aurora";
 
 
+const Workouts = () => {
+  return (
+    <div className={style.workoutsBody}>
 
-const  Workouts = () => {
-    return (
-        <>
-        
-        </>
-    );
-}
+      <section className={style.presentationWorkouts}>
+        <div className={style.auroraBg}>
+          <Aurora
+            colorStops={["#9C1107", "#0C0A09", "#9C1107"]}
+            blend={0.5}
+            amplitude={1.0}
+            speed={0.5}
+          />
+        </div>
+
+        <div className={style.workoutsContainer}>
+
+            <h1>Edita y organiza tus rutinas</h1>
+
+        </div>
+
+      </section>
+
+    </div>
+
+  );
+};
 
 export default Workouts;
